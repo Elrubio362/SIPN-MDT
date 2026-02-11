f8b0a521a28188d0b59adbfbc64fad9626b73cc6
Elrubio362 <eresuncalvotontoypunto@gmail.com>
2026-02-11 20:31:20 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-function login(){
-    const u=document.getElementById('user').value;
-    const p=document.getElementById('pass').value;
-    if(u==="Pol Beltran" && p==="Polbeltran5."){
-        document.getElementById('loginScreen').classList.add('hidden');
-        document.getElementById('hub').classList.remove('hidden');
-    }else{
-        alert("Credenciales incorrectas");
-    }
-}
+console.log('SIPN V30');
