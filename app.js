ed246d65594fb9dd4ca5152080e45d39854a1fb0
Elrubio362 <eresuncalvotontoypunto@gmail.com>
2026-02-11 22:25:53 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 
 function login(){
-  const u=document.getElementById('user').value;
-  const p=document.getElementById('pass').value;
-  if(u==="Pol Beltran" && p==="Polbeltran5"){
-    document.getElementById("loginScreen").style.display="none";
-    document.getElementById("mdtScreen").style.display="block";
-  }else{
-    alert("Credenciales incorrectas");
-  }
+ const u=document.getElementById('user').value;
+ const p=document.getElementById('pass').value;
+ if(u==='Pol Beltran' && p==='Polbeltran5.'){
+   document.getElementById('loginScreen').style.display='none';
+   document.getElementById('hub').style.display='block';
+ } else {
+   document.getElementById('msg').innerText='Credenciales incorrectas';
+ }
 }
-function openModule(id){
-  document.querySelectorAll('.module').forEach(m=>m.style.display='none');
-  document.getElementById(id).style.display='block';
+function showPage(id){
+ document.getElementById('hub').style.display='none';
+ document.getElementById(id).style.display='block';
+}
+function back(){
+ document.querySelectorAll('.page').forEach(p=>p.style.display='none');
+ document.getElementById('hub').style.display='block';
 }
