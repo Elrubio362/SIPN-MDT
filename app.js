899a7e6793134f6468da3d752270ebd1fbfb0e1f
Elrubio362 <eresuncalvotontoypunto@gmail.com>
2026-02-11 20:24:30 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
 function login(){
     const u=document.getElementById('user').value;
     const p=document.getElementById('pass').value;
-    if(u==='Pol Beltran' && p==='Polbeltran5.'){
+    if(u==="Pol Beltran" && p==="Polbeltran5."){
         document.getElementById('loginScreen').classList.add('hidden');
         document.getElementById('hub').classList.remove('hidden');
     }else{
-        document.getElementById('error').innerText='Credenciales incorrectas';
+        alert("Credenciales incorrectas");
     }
 }
