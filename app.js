bab4797215266b04b420803ea70b236745bc9dab
Elrubio362 <eresuncalvotontoypunto@gmail.com>
2026-02-12 01:27:45 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-function login(){
-  const u=document.getElementById('user').value.trim();
-  const p=document.getElementById('pass').value.trim();
-  if(u==="Pol Beltran" && p==="Polbeltran5"){
-    document.getElementById("loginPage").style.display="none";
-    document.getElementById("hubPage").style.display="block";
-  }else{
-    document.getElementById("msg").innerText="Credenciales incorrectas";
-  }
-}
+// app placeholder with random citizens & vehicles note
